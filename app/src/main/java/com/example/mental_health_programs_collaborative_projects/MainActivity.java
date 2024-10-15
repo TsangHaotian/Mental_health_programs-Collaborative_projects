a2ac59ba98ca9b1ca18d4473231dd041821f3ecd
Chiang-Link <441978169@qq.com>
2024-10-16 03:16:07 +0800

    蒋丽颖
    在values当中增加了styles.xml布局，让mainactivity页面使用该xml文件，增加了一行Java代码在mainactivity对应的Java文件里面，并且删掉了一行系统自动生成的代码，实现了状态栏的透明

--- app/src/main/java/com/example/mental_health_programs_collaborative_projects/MainActivity.java
+++ app/src/main/java/com/example/mental_health_programs_collaborative_projects/MainActivity.java
@@ -43,11 +43,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+//      使状态栏变成透明颜色
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
 
         //初始化控件
         BottomNavigationView mbottomNavigationView = findViewById(R.id.bt_bottomnavigation);
