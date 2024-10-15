3c657992d380140efc10ee8d48ab70fc86799bb8
TsangHaotian <TsangHaotian@hotmail.com>
2024-10-16 03:13:05 +0800

    曾昊天
    删除了某些代码

--- app/src/main/java/com/example/mental_health_programs_collaborative_projects/MainActivity.java
+++ app/src/main/java/com/example/mental_health_programs_collaborative_projects/MainActivity.java
@@ -43,11 +43,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         //初始化控件
         BottomNavigationView mbottomNavigationView = findViewById(R.id.bt_bottomnavigation);
